b3012700c9cb572926969dcebb2ced39fe94c206
oggyyy2000 <tuanbeos47@gmail.com>
2023-06-19 17:45:18 +0700

    add new color

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-grayish-blue": "#dae3f3",
+      },
+    },
+  },
+  plugins: [],
+};
